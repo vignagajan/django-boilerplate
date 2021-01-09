955a6ed9258f785e4a3cf19b031f37137ad3e9ff
vignagajan <my.soclntwrks@gmail.com>
2021-01-09 13:24:09 +0530

    Create privacy and terms

--- conf/urls.py
+++ conf/urls.py
@@ -4,6 +4,8 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('privacy/', TemplateView.as_view(template_name='privacy-policy.html'), name='privacy'),
+    path('terms/', TemplateView.as_view(template_name='terms-of-use.html'), name='terms'),
     path('', include('apps.users.urls')),
     path('dashboard/', TemplateView.as_view(template_name='dashboard.html'), name='dashboard'),
 ]
