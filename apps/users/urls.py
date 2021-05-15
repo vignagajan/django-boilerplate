640a66d18c2bd8669855bf1e7b7a7ddb8099c6fa
vignagajan <my.soclntwrks@gmail.com>
2021-05-15 15:52:01 +0530

    Updarade UI to bootstrap5

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('login/',auth_views.LoginView.as_view(),name='login'),
     path('logout/',auth_views.LogoutView.as_view(),name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('forgot/', auth_views.PasswordChangeView.as_view(), name='forgot'),
 ]
